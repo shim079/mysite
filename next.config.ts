f12cd62aafae4f19890ca9d5958733ae4d663c56
shim079 <150309430+shim079@users.noreply.github.com>
2025-07-26 13:58:15 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  module.exports = {
+  basePath: '/mysite',
+  assetPrefix: '/mysite/',
+}
 };
 
 export default nextConfig;
