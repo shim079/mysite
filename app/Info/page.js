cd2f3dc2920ade262b564bd596200901e1ce8350
shim079 <150309430+shim079@users.noreply.github.com>
2025-07-27 17:10:34 +0300

    Update page.js

--- app/Info/page.js
+++ app/Info/page.js
@@ -7,16 +7,16 @@ import SpotlightCard from './SpotlightCard';
 
 
 const page = () => {
-  return (
+ return (
     <div className={styles.all}>
-     <div className=' p-30 pb-95  '>
+     <div className='p-10 md:p-30 pb-95  '>
                 <div className='mb-10'>
-                    <h2  className=' relative z-10 pl-30 pt-35 text-white text-4xl text-shadow-lg'>Hi, I’M SHAIMA <span className="multiple-text"></span></h2>
-                    <h3 className=' relative z-10 pl-30 mt-3 text-white text-3xl '>Graphic Designer, Full Stack Developer, And a Self-Learner</h3>
-                    <h5 className=' relative z-10 pl-30 mt-3 text-white text-1'>I craft intutive, beautiful, and accessible mobile experiences. Passionate about turning ideas into apps that peaple love.</h5>
+                    <h2  className=' relative z-10 xl:pl-30 pt-35 text-white xl:text-4xl text-3xl text-shadow-lg '>Hi, I’M SHAIMA <span className="multiple-text"></span></h2>
+                    <h3 className=' relative z-10 xl:pl-30 mt-3 text-white xl:text-3xl text-2xl '>Graphic Designer, Full Stack Developer, And a Self-Learner</h3>
+                    <h5 className=' relative z-10 xl:pl-30 mt-3 text-white text-1'>I craft intutive, beautiful, and accessible mobile experiences. Passionate about turning ideas into apps that peaple love.</h5>
                 </div>
     
-                <div className='text-1xl text-white ml-30 flex items-center space-x-9 
+                <div className='text-1xl text-white xl:ml-30 flex items-center space-x-9 
                  absolute '>
                     <a href='https://www.linkedin.com/in/shaima-alharbi-7201b822a/'><FaLinkedinIn /></a>
                     <a href='/'><FaTwitter /></a>
@@ -33,9 +33,8 @@ const page = () => {
                 
                 
     
-    <SpotlightCard className=" w-50 ml-30 mt-25 box-content size-30  content-center  cursor-pointer text-center 
-    h-6 text-white [transition:0.5s] rounded-3xl p-1 absolute 
-                   custom-spotlight-card">
+    <SpotlightCard className=" w-50 xl:ml-30 mt-25 box-content size-30  content-center  cursor-pointer text-center 
+    h-6 text-white [transition:0.5s] rounded-3xl p-1 absolute custom-spotlight-card">
                 
                     <Link
                         href="/Project"> 
