1ed9fbd4e3a8861419a8ef6847b36410878550a2
shim079 <150309430+shim079@users.noreply.github.com>
2025-07-27 17:42:37 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   basePath: '/mysite',
   assetPrefix: '/mysite/',
-  // Optional: if you use images or static assets
   images: {
     unoptimized: true,
   },
-  // Optional: if deploying static export
   output: 'export',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
